33126e46012571fb3dedde5389823622f27f3aa6
mkaif8417 <mkaif6937@gmail.com>
2026-05-31 01:59:42 +0530

    width

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,24 +5,31 @@ import Footer from "./components/layout/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-[var(--qode-background-color)] text-white p-4 md:p-6 lg:p-8">
-
       {/* Luxury Frame */}
       <div className="relative min-h-[calc(100vh-2rem)] fidalgo-frame md:min-h-[calc(100vh-3rem)]">
+{/* Vertical Lines */}
+<div className="absolute top-20 bottom-20 left-18 w-px bg-[#C9A581]/20"></div>
+<div className="absolute top-20 bottom-20 right-18 w-px bg-[#C9A581]/20"></div>
 
-        {/* Top Left */}
-        <div className="absolute top-6 left-26 w-12 h-[2px] md:w-16  md:h-16 border-t border-l border-[#C9A581]/70"></div>
+{/* Horizontal Lines */}
+<div className="absolute left-20 right-20 top-6 h-px bg-[#C9A581]/20"></div>
+<div className="absolute left-20 right-20 bottom-6 h-px bg-[#C9A581]/20"></div>
 
-        {/* Top Right */}
-        <div className="absolute top-6 right-16 w-12 md:w-16 h-12 md:h-16 border-t border-r border-[#C9A581]/70"></div>
+{/* Corners */}
+{/* left top */}
+<div className="absolute top-4 left-18 w-20 h-20 border-t border-l border-[#C9A581]"></div>
+<div className="absolute top-8 left-22 w-10 h-10 border-t border-l border-[#C9A581]"></div>
+{/* right top */}
+<div className="absolute top-4 right-18 w-20 h-20 border-t border-r border-[#C9A581]"></div>
+<div className="absolute top-8 right-22 w-10 h-10 border-t border-r border-[#C9A581]"></div>
+{/* bottom left */}
+<div className="absolute bottom-8 left-18 w-20 h-20 border-b border-l border-[#C9A581]"></div>
+<div className="absolute bottom-12 left-22 w-10 h-10 border-b border-l border-[#C9A581]"></div>
+{/* bottom right */}
+<div className="absolute bottom-8 right-18 w-20 h-20 border-b border-r border-[#C9A581]"></div>
+<div className="absolute bottom-12 right-22 w-10 h-10 border-b border-r border-[#C9A581]"></div>
 
-        {/* Bottom Left */}
-        <div className="absolute bottom-12 left-12 w-12 md:w-16 h-12 md:h-16 border-b border-l border-[#C9A581]/70"></div>
 
-        {/* Bottom Right */}
-        <div className="absolute bottom-12 right-12 w-12 md:w-16 h-12 md:h-16 border-b border-r border-[#C9A581]/70"></div>
-
-        {/* Thin Border */}
-        <div className="absolute inset-0 border border-[#C9A581]/20 pointer-events-none"></div>
 
         {/* Content */}
         <div className="relative px-6 md:px-10 lg:px-14">
